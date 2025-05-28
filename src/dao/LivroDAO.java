de87f300a69ddbb39524e20163906ed56b9a69a5
Iran <iranildosousa1234@gmail.com>
2025-05-28 11:39:58 -0300

    CRUD livro Completo - Classe - LivroDAO

---
+++ src/dao/LivroDAO.java
@@ -0,0 +1,93 @@
+package dao;
+
+import model.Livro;
+import util.DataBaseConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LivroDAO {
+
+    DataBaseConnection dataBaseConnection;
+
+    //CREATE
+    public void create(Livro livro){
+
+        //comando SQL para rodar
+        String sql = "INSERT INTO Livros(titulo, autor, ano_publicacao, quantidade_estoque) VALUES (?,?,?,?)";
+
+        //Fazer TryCatch para tratar o error do SQL
+        try(Connection connection = dataBaseConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+            preparedStatement.setString(1, livro.getTitulo());
+            preparedStatement.setString(2, livro.getAutor());
+            preparedStatement.setInt(3, livro.getAno_publicacao());
+            preparedStatement.setInt(4, livro.getQuantidade_estoque());
+            preparedStatement.executeUpdate();
+
+            System.out.println("Livro inserido com sucesso!");
+        } catch (SQLException error){
+            error.printStackTrace();
+        }
+    }
+
+    //READ
+    public List<Livro> read(){
+        List<Livro> livros = new ArrayList<>();
+        String sql = "SELECT * FROM livros";
+
+        try (Connection connection = dataBaseConnection.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)){
+
+            while(resultSet.next()){
+                Livro livro = new Livro(
+                        resultSet.getString("titulo"),
+                        resultSet.getNString("autor"),
+                        resultSet.getInt("ano_publicacao"),
+                        resultSet.getInt("quantidade_estoque")
+                );
+                livros.add(livro);
+            }
+        } catch (SQLException error){
+            error.printStackTrace();
+        }
+        return livros;
+    }
+
+    //UPDATE
+    public void update(Livro livro){
+        String sql = "UPDATE Alunos SET titulo = ?, autor = ?, ano_publicacao = ?, quantidate_estoque = ? WHERE id_livro = ?";
+
+        try(Connection connection = dataBaseConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setString(1, livro.getTitulo());
+            preparedStatement.setString(2, livro.getAutor());
+            preparedStatement.setInt(3, livro.getAno_publicacao());
+            preparedStatement.setInt(4, livro.getQuantidade_estoque());
+            preparedStatement.setInt(5, livro.getId());
+            preparedStatement.executeUpdate();
+
+            System.out.println("Livro atualizado com sucesso!");
+        } catch (SQLException error){
+            error.printStackTrace();
+        }
+    }
+
+    //DELETE
+    public void delete(int id){
+        String sql = "DELETE FROM Livros where id_livro = ?";
+
+        try(Connection connection = dataBaseConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+            System.out.println("Livro deletado com sucesso!");
+        } catch (SQLException error){
+            error.printStackTrace();
+        }
+    }
+}
---
+++ src/model/Livro.java
@@ -0,0 +1,65 @@
+package model;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private int ano_publicacao;
+    private int quantidade_estoque;
+
+    public Livro(){}
+
+    public Livro(String titulo, String autor, int ano_publicacao, int quantidade_estoque){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano_publicacao = ano_publicacao;
+        this.quantidade_estoque = quantidade_estoque;
+    }
+    public Livro(int id, String titulo, String autor, int ano_publicacao, int quantidade_estoque) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano_publicacao = ano_publicacao;
+        this.quantidade_estoque = quantidade_estoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno_publicacao() {
+        return ano_publicacao;
+    }
+
+    public void setAno_publicacao(int ano_publicacao) {
+        this.ano_publicacao = ano_publicacao;
+    }
+
+    public int getQuantidade_estoque() {
+        return quantidade_estoque;
+    }
+
+    public void setQuantidade_estoque(int quantidade_estoque) {
+        this.quantidade_estoque = quantidade_estoque;
+    }
+}
